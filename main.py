83f3ab1f26d54c3edbed797ab32e240e65eec97d
PramitaPandit <78577352+PramitaPandit@users.noreply.github.com>
2021-09-08 17:54:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Rock-Paper-Scissor game
+#
+import random
+
+#step1: Stating game instructions
+print('Rules of Rock-Paper-Scissor')
+
+# Taking user input
+player_name = input('Enter your name: ')
+player_choice = input('Enter a choice (rock, paper, scissor')
